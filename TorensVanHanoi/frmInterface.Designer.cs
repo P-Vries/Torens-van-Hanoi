3ac68a3b790ce102e927e8c6070b8e2f0a2ab2c4
P-Vries <43247243+P-Vries@users.noreply.github.com>
2018-10-16 15:29:01 +0200

    Add files via upload

---
+++ TorensVanHanoi/frmInterface.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace TorensVanHanoi
+{
+    partial class frmInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbOut = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPilaar1 = new System.Windows.Forms.Button();
+            this.btnPillaar2 = new System.Windows.Forms.Button();
+            this.btnPilaar3 = new System.Windows.Forms.Button();
+            this.lblZetO = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbOut
+            // 
+            this.pbOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOut.Location = new System.Drawing.Point(12, 41);
+            this.pbOut.Name = "pbOut";
+            this.pbOut.Size = new System.Drawing.Size(1240, 628);
+            this.pbOut.TabIndex = 0;
+            this.pbOut.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPilaar1
+            // 
+            this.btnPilaar1.Enabled = false;
+            this.btnPilaar1.Location = new System.Drawing.Point(141, 675);
+            this.btnPilaar1.Name = "btnPilaar1";
+            this.btnPilaar1.Size = new System.Drawing.Size(75, 23);
+            this.btnPilaar1.TabIndex = 2;
+            this.btnPilaar1.Text = "Pilaar 1";
+            this.btnPilaar1.UseVisualStyleBackColor = true;
+            this.btnPilaar1.Click += new System.EventHandler(this.btnPilaar1_Click);
+            // 
+            // btnPillaar2
+            // 
+            this.btnPillaar2.Enabled = false;
+            this.btnPillaar2.Location = new System.Drawing.Point(558, 675);
+            this.btnPillaar2.Name = "btnPillaar2";
+            this.btnPillaar2.Size = new System.Drawing.Size(75, 23);
+            this.btnPillaar2.TabIndex = 3;
+            this.btnPillaar2.Text = "Pilaar 2";
+            this.btnPillaar2.UseVisualStyleBackColor = true;
+            this.btnPillaar2.Click += new System.EventHandler(this.btnPillaar2_Click);
+            // 
+            // btnPilaar3
+            // 
+            this.btnPilaar3.Enabled = false;
+            this.btnPilaar3.Location = new System.Drawing.Point(971, 675);
+            this.btnPilaar3.Name = "btnPilaar3";
+            this.btnPilaar3.Size = new System.Drawing.Size(75, 23);
+            this.btnPilaar3.TabIndex = 4;
+            this.btnPilaar3.Text = "Pilaar 3";
+            this.btnPilaar3.UseVisualStyleBackColor = true;
+            this.btnPilaar3.Click += new System.EventHandler(this.btnPilaar3_Click);
+            // 
+            // lblZetO
+            // 
+            this.lblZetO.AutoSize = true;
+            this.lblZetO.Location = new System.Drawing.Point(93, 17);
+            this.lblZetO.Name = "lblZetO";
+            this.lblZetO.Size = new System.Drawing.Size(75, 13);
+            this.lblZetO.TabIndex = 5;
+            this.lblZetO.Text = "Aantal zetten: ";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1177, 675);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 708);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblZetO);
+            this.Controls.Add(this.btnPilaar3);
+            this.Controls.Add(this.btnPillaar2);
+            this.Controls.Add(this.btnPilaar1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pbOut);
+            this.Name = "frmInterface";
+            this.Text = "Torens van Hanoi";
+            ((System.ComponentModel.ISupportInitialize)(this.pbOut)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbOut;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPilaar1;
+        private System.Windows.Forms.Button btnPillaar2;
+        private System.Windows.Forms.Button btnPilaar3;
+        private System.Windows.Forms.Label lblZetO;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ TorensVanHanoi/frmInterface.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorensVanHanoi
+{
+    public partial class frmInterface : Form
+    {
+        //
+        //Vars
+        //
+        List<Rechthoek> pilaar1 = new List<Rechthoek>();
+        List<Rechthoek> pilaar2 = new List<Rechthoek>();
+        List<Rechthoek> pilaar3 = new List<Rechthoek>();
+        Rechthoek r1 = new Rechthoek(100);
+        Rechthoek r2 = new Rechthoek(120);
+        Rechthoek r3 = new Rechthoek(140);
+        Rechthoek r4 = new Rechthoek(160);
+        Rechthoek r5 = new Rechthoek(180);
+        Rechthoek r6 = new Rechthoek(200);
+        Rechthoek r7 = new Rechthoek(220);
+        Rechthoek r8 = new Rechthoek(240);
+        Pen potlood = new Pen(Color.Gray, 10);
+        SolidBrush brush = new SolidBrush(Color.Green);
+        Graphics papier;
+        bool clicked = false;
+        bool pilar1Selected = false;
+        bool pilar2Selected = false;
+        bool pilar3Selected = false;
+        bool isFirstPilaar1 = false;
+        bool isFisrtPilaar2 = true;
+        bool isFirstPilaar3 = true;
+        int Zet = 0;
+
+        public frmInterface()
+        {
+            InitializeComponent();
+
+        }
+        //
+        //Me3thodes
+        //
+        private void Laad()
+        {
+            papier = pbOut.CreateGraphics();
+            for (int i = 1; i <= 8; i++)
+            {
+                if (i == 8)
+                {
+                    pilaar1.Add(r1);
+                }
+                if (i == 7)
+                {
+                    pilaar1.Add(r2);
+                }
+                if (i == 6)
+                {
+                    pilaar1.Add(r3);
+                }
+                if (i == 5)
+                {
+                    pilaar1.Add(r4);
+                }
+                if (i == 4)
+                {
+                    pilaar1.Add(r5);
+                }
+                if (i == 3)
+                {
+                    pilaar1.Add(r6);
+                }
+                if (i == 2)
+                {
+                    pilaar1.Add(r7);
+                }
+                if (i == 1)
+                {
+                    pilaar1.Add(r8);
+                }
+
+            }
+            Point punt = new Point(60, 500);
+            for (int i = 0; i <8; i++)
+            {
+                papier.DrawRectangle(potlood, pilaar1[i].ToRectangle(punt));
+                papier.FillRectangle(brush, pilaar1[i].ToRectangle(punt));
+                punt = new Point(punt.X + 10, punt.Y - 60);
+            }
+
+        }
+
+        private void Teken()
+        {
+            papier = pbOut.CreateGraphics();
+            papier.Clear(pbOut.BackColor);
+            Point pilar1 = new Point(60, 500);
+            Point pilar2 = new Point(500, 500);
+            Point pilar3 = new Point(900, 500);
+            for (int i = 0; i < pilaar1.Count; i++)
+            {
+                papier.DrawRectangle(potlood, pilaar1[i].ToRectangle(pilar1));
+                papier.FillRectangle(brush, pilaar1[i].ToRectangle(pilar1));
+                pilar1 = new Point(pilar1.X + 10, pilar1.Y - 60);
+            }
+            for (int i = 0; i < pilaar2.Count; i++)
+            {
+                papier.DrawRectangle(potlood, pilaar2[i].ToRectangle(pilar2));
+                papier.FillRectangle(brush, pilaar2[i].ToRectangle(pilar2));
+                pilar2 = new Point(pilar2.X + 10, pilar2.Y - 60);
+            }
+            for (int i = 0; i < pilaar3.Count; i++)
+            {
+                papier.DrawRectangle(potlood, pilaar3[i].ToRectangle(pilar3));
+                papier.FillRectangle(brush, pilaar3[i].ToRectangle(pilar3));
+                pilar3 = new Point(pilar3.X + 10, pilar3.Y - 60);
+            }
+        }
+        private void ListVerplaats(byte pilaarnummer)
+        {
+            if(pilaarnummer == 1)
+            {
+                if (pilar2Selected)
+                {
+                    if(isFirstPilaar1)
+                    {
+                        pilaar1.Add(pilaar2.Last());
+                        pilaar2.RemoveAt(pilaar2.Count - 1);
+                        isFirstPilaar1 = false;
+                    }
+                    else
+                    {
+                        if (pilaar1.Last().Size.Width > pilaar2.Last().Size.Width)
+                        {
+                            pilaar1.Add(pilaar2.Last());
+                            pilaar2.RemoveAt(pilaar2.Count - 1);
+                        }
+                        else MessageBox.Show("Deze zet is ongeldig");
+                    }
+                }
+                if (pilar3Selected)
+                {
+                    if (isFirstPilaar1)
+                    {
+                        pilaar1.Add(pilaar3.Last());
+                        pilaar3.RemoveAt(pilaar3.Count - 1);
+                        isFirstPilaar1 = false;
+                    }
+                    else
+                    {
+                        if (pilaar1.Last().Size.Width > pilaar3.Last().Size.Width)
+                        {
+                            pilaar1.Add(pilaar3.Last());
+                            pilaar3.RemoveAt(pilaar3.Count - 1);
+                        }
+                    }
+                }
+                if (pilar1Selected)MessageBox.Show("Deze Pilaar is al ingeklikt");
+                BooleanSwitch();
+            }
+            if(pilaarnummer == 2)
+            {
+                if (pilar1Selected)
+                {
+                    if (isFisrtPilaar2)
+                    {
+                        pilaar2.Add(pilaar1.Last());
+                        pilaar1.RemoveAt(pilaar1.Count - 1);
+                        isFisrtPilaar2 = false;
+                    }
+                    else
+                    {
+                        if (pilaar2.Last().Size.Width > pilaar1.Last().Size.Width)
+                        {
+                            pilaar2.Add(pilaar1.Last());
+                            pilaar1.RemoveAt(pilaar1.Count - 1);
+                        }
+                        else MessageBox.Show("Deze zet is ongeldig");
+                    }
+                }
+                if (pilar3Selected)
+                {
+                    if (isFisrtPilaar2)
+                    {
+                        pilaar2.Add(pilaar3.Last());
+                        pilaar3.RemoveAt(pilaar3.Count - 1);
+                    }
+                    else
+                    {
+                        if(pilaar2.Last().Size.Width > pilaar3.Last().Size.Width)
+                        {
+                            pilaar2.Add(pilaar3.Last());
+                            pilaar3.RemoveAt(pilaar3.Count - 1);
+                        }
+                    }
+                }               
+                if (pilar2Selected) MessageBox.Show("Deze Pilaar is al ingeklikt");
+                BooleanSwitch();
+            }
+            if(pilaarnummer == 3)
+            {
+                if (pilar1Selected)
+                {
+                    if (isFirstPilaar3)
+                    {
+                        pilaar3.Add(pilaar1.Last());
+                        pilaar1.RemoveAt(pilaar1.Count - 1);
+                        isFirstPilaar3 = false;
+                    }
+                    else
+                    {
+                        if (pilaar3.Last().Size.Width > pilaar1.Last().Size.Width)
+                        {
+                            pilaar3.Add(pilaar1.Last());
+                            pilaar1.RemoveAt(pilaar1.Count - 1);
+                        }
+                        else MessageBox.Show("Deze zet is ongeldig");
+                    }
+                }
+                if (pilar2Selected)
+                {
+                    if (isFirstPilaar3)
+                    {
+                        pilaar3.Add(pilaar2.Last());
+                        pilaar2.RemoveAt(pilaar2.Count - 1);
+                        isFirstPilaar3 = false;
+                    }
+                    else
+                    {
+                        if (pilaar3.Last().Size.Width > pilaar2.Last().Size.Width)
+                        {
+                            pilaar3.Add(pilaar2.Last());
+                            pilaar2.RemoveAt(pilaar2.Count - 1);
+                        }
+                        else MessageBox.Show("Deze zet is ongeldig");
+                    }
+
+                }
+                if (pilar3Selected) MessageBox.Show("Deze Pilaar is al ingeklikt");
+                BooleanSwitch();
+            }
+            BooleanSwitch();
+        }
+        private void BooleanSwitch()
+        {
+            clicked = false;
+            pilar1Selected = false;
+            pilar2Selected = false;
+            pilar3Selected = false;
+            if(pilaar1.Count == 0) { isFirstPilaar1 = true; }
+            if(pilaar2.Count == 0) { isFisrtPilaar2 = true; }
+            if(pilaar3.Count == 0) { isFirstPilaar3 = true; }
+        }
+
+        private void Verplaats(byte pilaarnummer)
+        {
+            if (clicked == false)
+            {
+                if (pilaarnummer == 1)
+                {
+                    if (pilaar1.Count != 0)
+                    {
+                        pilar1Selected = true;
+                        clicked = true;
+                    }
+                    else MessageBox.Show("Er valt hier niets te tillen!");
+
+                }
+                if (pilaarnummer == 2)
+                {
+                    if (pilaar2.Count != 0)
+                    {
+                        pilar2Selected = true;
+                        clicked = true;
+                    }
+                    else MessageBox.Show("Er valt hier niets te tillen!");
+
+                }
+                if (pilaarnummer == 3)
+                {
+                    if (pilaar3.Count != 0)
+                    {
+                        pilar3Selected = true;
+                        clicked = true;
+                    }
+                    else MessageBox.Show("Er valt hier niets te tillen!");
+
+                }
+            }
+            else if (clicked)
+            {
+
+                ListVerplaats(pilaarnummer);
+                Teken();
+                Zet++;
+                lblZetO.Text = "Aantal zetten: " + Zet.ToString();
+            }
+            if (pilaar2.Count == 8 || pilaar3.Count == 8)
+            {
+                Win();
+            }
+        }
+        private void Win()
+        {
+            MessageBox.Show("Gefeliciteerd met het winnen!");
+        }
+        //
+        //Events
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Laad();
+            btnPilaar1.Enabled = true;
+            btnPillaar2.Enabled = true;
+            btnPilaar3.Enabled = true;
+            btnStart.Enabled = false;
+        }
+
+        private void btnPilaar1_Click(object sender, EventArgs e)
+        {
+            Verplaats(1);
+        }
+
+        private void btnPillaar2_Click(object sender, EventArgs e)
+        {
+            Verplaats(2);
+        }
+
+        private void btnPilaar3_Click(object sender, EventArgs e)
+        {
+            Verplaats(3);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    public class Rechthoek
+    {
+
+        //
+        //Constructors
+        //
+
+        public Rechthoek()
+        {
+            Size = new Size(20, 10);
+            _kleur = Color.Green;
+
+        }
+
+        public Rechthoek(int grootte)
+        {
+            Size = new Size(grootte, 50);
+            _kleur = Color.Green;
+        }
+
+        //
+        //Methodes
+        //
+        public Rectangle ToRectangle(Point location)
+        {
+            Rectangle rect = new Rectangle(location, Size);
+
+            return rect;
+        }
+
+
+        //
+        //Prive Vars
+        //
+        Color _kleur;
+
+        //
+        //Get-Setters
+        //
+        public Color Kleur { get { return _kleur; } }
+        public Size Size { get; set; }
+    }
+
+}
